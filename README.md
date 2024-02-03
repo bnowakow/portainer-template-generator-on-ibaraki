65bcadaa50a994b7f1c4016b542c8d85ce9ed40c
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2024-02-03 16:47:03 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,3 @@
+https://www.reddit.com/r/portainer/comments/14m11d8/500_portainer_template_multi_url_templates/ describes tool that merges multiple portainer template.json into one
+This is just small script to save url list of them 
+
---
+++ generate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+gnerator_url="https://templates-portainer.ibaraki.app/?templates="
+
+encoded_list=$(base64 -i template-list.json)
+
+url="$gnerator_url$encoded_list"
+
+curl $url | jq '.' > template.json
+
+echo $url 
+
